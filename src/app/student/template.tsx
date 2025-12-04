403542b5a749d2c964ef12d064ed47f737aab0f6
Rushil Mahadevu <rushil.mahadevu@gmail.com>
2025-12-04 06:50:21 -0800

    transitions blur

--- src/app/student/template.tsx
+++ src/app/student/template.tsx
@@ -5,9 +5,9 @@ import { motion } from "framer-motion";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.3, ease: "easeInOut" }}
+      initial={{ opacity: 0, filter: "blur(5px)" }}
+      animate={{ opacity: 1, filter: "blur(0px)" }}
+      transition={{ duration: 0.4, ease: "easeOut" }}
     >
       {children}
     </motion.div>
